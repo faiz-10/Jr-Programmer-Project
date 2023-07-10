70a7c15641832d36f36dd3498f8ce9350728f8ba
faiz-10 <96327887+faiz-10@users.noreply.github.com>
2023-07-10 14:31:57 +0530

    final commit
    completed warehouse prototype

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 // Sets the script to be executed later than all default scripts
 // This is helpful for UI, since other things may need to be initialized before setting the UI
@@ -9,10 +11,15 @@ using UnityEngine;
 public class MenuUIHandler : MonoBehaviour
 {
     public ColorPicker ColorPicker;
+    public static Color colorSelected;
 
     public void NewColorSelected(Color color)
     {
         // add code here to handle when a color is selected
+        // transfer this color to next scene;
+        MainManager.Instance.TeamColor = color;
+        Debug.Log("color selected " + color);
+        
     }
     
     private void Start()
@@ -20,5 +27,37 @@ public class MenuUIHandler : MonoBehaviour
         ColorPicker.Init();
         //this will call the NewColorSelected function when the color picker have a color button clicked.
         ColorPicker.onColorChanged += NewColorSelected;
+        ColorPicker.SelectColor(MainManager.Instance.TeamColor);
     }
+    public void SwithScene()
+    {
+        SceneManager.LoadScene(sceneName: "Main");
+    }
+    
+    public void Exit()
+    {
+        MainManager.Instance.SaveColor();
+        //conditional compiling
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#endif
+#if !UNITY_EDITOR
+        Application.Quit();
+#endif
+        
+        //Application.Quit();(works for built applications only)
+    }
+
+    public void SaveColorClicked()
+    {
+        MainManager.Instance.SaveColor();
+    }
+
+    public void LoadColorClicked()
+    {
+        MainManager.Instance.LoadColor();
+        ColorPicker.SelectColor(MainManager.Instance.TeamColor);
+    }
+
+
 }
--- Assets/Scripts/UI/UIMainScene.cs
+++ Assets/Scripts/UI/UIMainScene.cs
@@ -73,4 +73,9 @@ public class UIMainScene : MonoBehaviour
             InfoPopup.Name.text = content.GetName();
         }
     }
+
+    public void SwitchScene()
+    {
+        SceneManager.LoadScene(sceneName: "Menu");
+    }
 }
